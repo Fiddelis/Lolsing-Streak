8a06a0b38effd0b5f29f241d483131af5bff00ff
Fiddelis <lucas.ruan@ges.inatel.br>
2025-05-27 20:29:49 -0300

    fix: backend url change to prod

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://lolsing-streak.onrender.com";
 
 export const getAccounts = async () => {
   const response = await fetch(`${BASE_URL}/account`);
